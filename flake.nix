50e9bff1ff46525a968856383a9e39c0abca7616
GTrunSec <gtrunsec@hardenedlinux.org>
2021-02-19 00:11:19 -0800

    multi-arch

--- flake.nix
+++ flake.nix
@@ -5,37 +5,43 @@
     next-terminal = { url = "github:dushixiang/next-terminal"; flake = false;};
   };
 
-  outputs = inputs@{ self, nixpkgs, ranz2nix, next-terminal }: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux";};
-      let
-        static = callPackage ./static.nix { inherit ranz2nix next-terminal;};
-        guacamole = callPackage ./guacamole.nix { };
-        src = pkgs.fetchFromGitHub {
-          owner = "dushixiang";
-          repo = "next-terminal";
-          rev = next-terminal.rev;
-          sha256 = next-terminal.narHash;
-        };
-      in
-        buildGoModule {
-          name = "next-terminal";
-          inherit src;
-          goPackagePath = "github.com/dushixiang/next-terminal";
-          subPackages = [ "pkg" ];
-          buildInputs = [ gcc pkgconfig ]
-                        ++ lib.optionals stdenv.isLinux [ stdenv.cc.libc.out ]
-                        ++ lib.optionals (stdenv.hostPlatform.libc == "glibc") [ stdenv.cc.libc.static ];
-          vendorSha256 = "sha256-KWv5ErnsGILcIHu/HgXJTomQYYoFqNmxs1ZC6gQlfK0=";
-          GOARCH = if stdenv.isDarwin then "amd64"
-                   else if stdenv.hostPlatform.system == "i686-linux" then "386"
-                   else if stdenv.hostPlatform.system == "x86_64-linux" then "amd64"
-                   else if stdenv.isAarch32 then "arm"
-                   else throw "Unsupported system";
-          CC = if stdenv.isDarwin then "clang" else "cc";
+  outputs = inputs@{ self, nixpkgs, ranz2nix, next-terminal }:
+    let
+      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"];
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+      {
+        overlay = final: prev: {
+          next-terminal = with final;
+            (let
+              static = callPackage ./static.nix { inherit ranz2nix next-terminal;};
+              guacamole = callPackage ./guacamole.nix { };
+              src = pkgs.fetchFromGitHub {
+                owner = "dushixiang";
+                repo = "next-terminal";
+                rev = next-terminal.rev;
+                sha256 = next-terminal.narHash;
+              };
+            in
+              buildGoModule {
+                name = "next-terminal";
+                inherit src;
+                goPackagePath = "github.com/dushixiang/next-terminal";
+                subPackages = [ "pkg" ];
+                buildInputs = [ gcc pkgconfig ]
+                              ++ lib.optionals stdenv.isLinux [ stdenv.cc.libc.out ]
+                              ++ lib.optionals (stdenv.hostPlatform.libc == "glibc") [ stdenv.cc.libc.static ];
+                vendorSha256 = "sha256-KWv5ErnsGILcIHu/HgXJTomQYYoFqNmxs1ZC6gQlfK0=";
+                GOARCH = if stdenv.isDarwin then "amd64"
+                         else if stdenv.hostPlatform.system == "i686-linux" then "386"
+                         else if stdenv.hostPlatform.system == "x86_64-linux" then "amd64"
+                         else if stdenv.isAarch32 then "arm"
+                         else throw "Unsupported system";
+                CC = if stdenv.isDarwin then "clang" else "cc";
 
 
-          preConfigure = ''
+                preConfigure = ''
           substituteInPlace pkg/api/routes.go \
           --replace "web/build/index.html" "$out/web/build/index.html" \
           --replace "web/build/static" "$out/web/build/static" \
@@ -43,18 +49,24 @@
           --replace "web/build/logo.svg" "$out/web/build/logo.svg"
            '';
 
-          buildPhase = ''
+                buildPhase = ''
           go build
           '';
 
-          installPhase = ''
+                installPhase = ''
           mkdir -p $out/{bin,web}
           cp -r ${static} $out/web/build
           cp -r next-terminal $out/bin/next-terminal
           ln -s ${guacamole}/sbin/guacd $out/bin/guacd
           '';
 
+              });
         };
-    checks.x86_64-linux.build = self.defaultPackage.x86_64-linux;
-  };
+
+        defaultPackage = forAllSystems (system: (import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        }).next-terminal);
+        checks.x86_64-linux.build = self.defaultPackage.x86_64-linux;
+      };
 }
