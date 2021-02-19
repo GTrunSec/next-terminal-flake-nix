a38cf688716e5ee1ca2d22968b9af6c059ba78b1
GTrunSec <gtrunsec@hardenedlinux.org>
2021-02-19 12:11:01 -0800

    formatter

--- static.nix
+++ static.nix
@@ -9,23 +9,23 @@
 , ranz2nix
 }:
 let
-
   src = next-terminal;
 
   noderanz = callPackage ranz2nix {
 
     nodejs = nodejs-12_x;
     sourcePath = src + "/web/";
-    packageOverride = name: spec: if name == "minimist" && spec ? resolved && spec.resolved == "" then {
-      resolved = "file://" + (
-        toString (
-          fetchurl {
-            url = "https://registry.npmjs.org/minimist/-/minimist-1.2.0.tgz";
-            sha256 = "0w7jll4vlqphxgk9qjbdjh3ni18lkrlfaqgsm7p14xl3f7ghn3gc";
-          }
-        )
-      );
-    } else {};
+    packageOverride = name: spec:
+      if name == "minimist" && spec ? resolved && spec.resolved == "" then {
+        resolved = "file://" + (
+          toString (
+            fetchurl {
+              url = "https://registry.npmjs.org/minimist/-/minimist-1.2.0.tgz";
+              sha256 = "0w7jll4vlqphxgk9qjbdjh3ni18lkrlfaqgsm7p14xl3f7ghn3gc";
+            }
+          )
+        );
+      } else { };
   };
   node_modules = noderanz.patchedBuild;
 in
@@ -40,19 +40,19 @@ stdenv.mkDerivation {
   sourceRoot = "source/web";
 
   postUnpack = ''
-        chmod -R +rw .
-        '';
+    chmod -R +rw .
+  '';
 
   NODE_ENV = "production";
 
   buildPhase = ''
-          export HOME=$(mktemp -d)
-          ln -sf ${node_modules}/node_modules node_modules
-          ln -sf ${node_modules.lockFile} package-lock.json
-          npm run build
-        '';
+    export HOME=$(mktemp -d)
+    ln -sf ${node_modules}/node_modules node_modules
+    ln -sf ${node_modules.lockFile} package-lock.json
+    npm run build
+  '';
 
   installPhase = ''
     cp -rv build $out
-        '';
+  '';
 }
