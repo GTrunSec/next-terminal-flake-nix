72c0c4f7d4ce189a999bd4bd79ac96539d9759bb
GTrunSec <gtrunsec@hardenedlinux.org>
2021-02-19 12:08:45 -0800

    formatter

--- flake.nix
+++ flake.nix
@@ -1,21 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/7ff5e241a2b96fff7912b7d793a06b4374bd846c";
-    ranz2nix = { url = "github:andir/ranz2nix"; flake = false;};
-    next-terminal = { url = "github:dushixiang/next-terminal"; flake = false;};
+    ranz2nix = {
+      url = "github:andir/ranz2nix";
+      flake = false;
+    };
+    next-terminal = {
+      url = "github:dushixiang/next-terminal";
+      flake = false;
+    };
   };
 
   outputs = inputs@{ self, nixpkgs, ranz2nix, next-terminal }:
     let
-      supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"];
-      forAllSystems =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      supportedSystems =
+        [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
-      {
-        overlay = final: prev: {
-          next-terminal = with final;
-            (let
-              static = callPackage ./static.nix { inherit ranz2nix next-terminal;};
+    {
+      overlay = final: prev: {
+        next-terminal = with final;
+          (
+            let
+              static =
+                callPackage ./static.nix { inherit ranz2nix next-terminal; };
               guacamole = callPackage ./guacamole.nix { };
               src = pkgs.fetchFromGitHub {
                 owner = "dushixiang";
@@ -24,49 +33,61 @@
                 sha256 = next-terminal.narHash;
               };
             in
-              buildGoModule {
-                name = "next-terminal";
-                inherit src;
-                goPackagePath = "github.com/dushixiang/next-terminal";
-                subPackages = [ "pkg" ];
-                buildInputs = [ gcc pkgconfig ]
-                              ++ lib.optionals stdenv.isLinux [ stdenv.cc.libc.out ]
-                              ++ lib.optionals (stdenv.hostPlatform.libc == "glibc") [ stdenv.cc.libc.static ];
-                vendorSha256 = "sha256-KWv5ErnsGILcIHu/HgXJTomQYYoFqNmxs1ZC6gQlfK0=";
-                GOARCH = if stdenv.isDarwin then "amd64"
-                         else if stdenv.hostPlatform.system == "i686-linux" then "386"
-                         else if stdenv.hostPlatform.system == "x86_64-linux" then "amd64"
-                         else if stdenv.isAarch32 then "arm"
-                         else throw "Unsupported system";
-                CC = if stdenv.isDarwin then "clang" else "cc";
+            buildGoModule {
+              name = "next-terminal";
+              inherit src;
+              goPackagePath = "github.com/dushixiang/next-terminal";
+              subPackages = [ "pkg" ];
+              buildInputs = [ gcc pkgconfig ]
+                ++ lib.optionals stdenv.isLinux [ stdenv.cc.libc.out ]
+                ++ lib.optionals (stdenv.hostPlatform.libc == "glibc")
+                [ stdenv.cc.libc.static ];
+              vendorSha256 =
+                "sha256-KWv5ErnsGILcIHu/HgXJTomQYYoFqNmxs1ZC6gQlfK0=";
+              GOARCH =
+                if stdenv.isDarwin then
+                  "amd64"
+                else if stdenv.hostPlatform.system == "i686-linux" then
+                  "386"
+                else if stdenv.hostPlatform.system == "x86_64-linux" then
+                  "amd64"
+                else if stdenv.isAarch32 then
+                  "arm"
+                else
+                  throw "Unsupported system";
+              CC = if stdenv.isDarwin then "clang" else "cc";
+
+              preConfigure = ''
+                substituteInPlace pkg/api/routes.go \
+                --replace "web/build/index.html" "$out/web/build/index.html" \
+                --replace "web/build/static" "$out/web/build/static" \
+                --replace "web/build/favicon.ico" "$out/web/build/favicon.ico" \
+                --replace "web/build/logo.svg" "$out/web/build/logo.svg"
+              '';
+
+              buildPhase = ''
+                go build
+              '';
+
+              installPhase = ''
+                mkdir -p $out/{bin,web}
+                cp -r ${static} $out/web/build
+                cp -r next-terminal $out/bin/next-terminal
+                ln -s ${guacamole}/sbin/guacd $out/bin/guacd
+              '';
+
+            }
+          );
+      };
 
 
-                preConfigure = ''
-          substituteInPlace pkg/api/routes.go \
-          --replace "web/build/index.html" "$out/web/build/index.html" \
-          --replace "web/build/static" "$out/web/build/static" \
-          --replace "web/build/favicon.ico" "$out/web/build/favicon.ico" \
-          --replace "web/build/logo.svg" "$out/web/build/logo.svg"
-           '';
-
-                buildPhase = ''
-          go build
-          '';
-
-                installPhase = ''
-          mkdir -p $out/{bin,web}
-          cp -r ${static} $out/web/build
-          cp -r next-terminal $out/bin/next-terminal
-          ln -s ${guacamole}/sbin/guacd $out/bin/guacd
-          '';
-
-              });
-        };
-
-        defaultPackage = forAllSystems (system: (import nixpkgs {
+      defaultPackage = forAllSystems (system:
+        (import nixpkgs {
           inherit system;
           overlays = [ self.overlay ];
         }).next-terminal);
-        checks.x86_64-linux.build = self.defaultPackage.x86_64-linux;
-      };
+
+      checks.x86_64-linux.build = self.defaultPackage.x86_64-linux;
+
+    };
 }
--- guacamole.nix
+++ guacamole.nix
@@ -1,6 +1,25 @@
-{stdenv, fetchFromGitHub, autoreconfHook, cairo, libjpeg_turbo, libpng, libossp_uuid,
-  freerdp ,freerdpUnstable, pango, libssh2, libvncserver, libpulseaudio ,openssl, libvorbis, libwebp, pkgconfig, perl, libtelnet, inetutils,
-  makeWrapper}:
+{ stdenv
+, fetchFromGitHub
+, autoreconfHook
+, cairo
+, libjpeg_turbo
+, libpng
+, libossp_uuid
+, freerdp
+, freerdpUnstable
+, pango
+, libssh2
+, libvncserver
+, libpulseaudio
+, openssl
+, libvorbis
+, libwebp
+, pkgconfig
+, perl
+, libtelnet
+, inetutils
+, makeWrapper
+}:
 
 stdenv.mkDerivation rec {
   name = "guacamole-${version}";
@@ -12,14 +31,14 @@ stdenv.mkDerivation rec {
     rev = "90e15cb70635e8d13ebdefec9262701d2179983a";
     sha256 = "sha256-fUkWn+NPfWEr35Yrt+OBf3/baz1xYdpWVise336RrJo=";
   };
-  NIX_CFLAGS_COMPILE= [
+  NIX_CFLAGS_COMPILE = [
     "-Wno-error=format-truncation"
     "-Wno-error=format-overflow"
   ];
 
-  buildInputs = with stdenv; [ freerdp freerdpUnstable autoreconfHook pkgconfig cairo libpng libjpeg_turbo libossp_uuid pango libssh2 libvncserver libpulseaudio openssl libvorbis libwebp libtelnet perl makeWrapper];
+  buildInputs = with stdenv; [ freerdp freerdpUnstable autoreconfHook pkgconfig cairo libpng libjpeg_turbo libossp_uuid pango libssh2 libvncserver libpulseaudio openssl libvorbis libwebp libtelnet perl makeWrapper ];
 
-  propogatedBuildInputs = with stdenv; [ freerdp autoreconfHook pkgconfig cairo libpng libjpeg_turbo libossp_uuid freerdp pango libssh2 libvncserver libpulseaudio openssl libvorbis libwebp inetutils];
+  propogatedBuildInputs = with stdenv; [ freerdp autoreconfHook pkgconfig cairo libpng libjpeg_turbo libossp_uuid freerdp pango libssh2 libvncserver libpulseaudio openssl libvorbis libwebp inetutils ];
 
   patchPhase = ''
     substituteInPlace ./src/protocols/rdp/keymaps/generate.pl --replace "/usr/bin/env perl" "${perl}/bin/perl"
@@ -30,12 +49,12 @@ stdenv.mkDerivation rec {
 
   postInstall = ''
     wrapProgram $out/sbin/guacd --prefix LD_LIBRARY_PATH ":" $out/lib
-    '';
+  '';
 
   meta = with stdenv.lib; {
     description = "Clientless remote desktop gateway";
     homepage = "https://guacamole.incubator.apache.org/";
-    maintainers = [ tomberek gtunsec ];
+    maintainers = [ tomberek gtrunsec ];
     license = licenses.asl20;
     platforms = platforms.linux;
   };
